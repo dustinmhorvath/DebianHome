06b913caaf5e06575a67de9b73c9cd67b80f1ced
Dustin Horvath <dustinmhorvath@gmail.com>
2016-01-26 11:38:29 -0600

    Added some arbitration and abstraction. Hostname and some root filesystem name now gathered dynamically.

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 ﻿<?php
-  
+
 /*
 *   +------------------------------------------------------------------------------+
-*       SERVER STATUS SCRIPT                                                               
+*       SERVER STATUS SCRIPT
 *   +------------------------------------------------------------------------------+
-*/ 
+*/
 
 
 /*
@@ -12,39 +12,47 @@
 *        Configuration
 *   +------------------------------------------------------------------------------+
 */
-//Enter the hostname/dynamic DNS address for your server
-$hostname = "home.bertelson.me";
+$root_partition = chop(` df | grep /$ | awk {'print $(NF-5)'} `);
+// Leave secondary empty if you don't have another mount that you want to watch.
+$secondary = "";
+// Put the domain of sites for top bar here. You might need to specify these
+//  below if your configuration is more complicated.
+$domain = "nichnologist.net";
 
-//Temperature readouts -- enter C or F (Default: C)
-$degtype = "F";
 
 // Define service checks
 $services = Array(
-	Array("80", 				"Internet Connection", 					"google.com"),
-	Array("80",					"HTTP (Hyper Text Transfer Protocol)", 	""),
-	Array("php5-fpm",		 	"PHP5-FPM", 							""),
-	Array("3306", 				"MySQL (Database server)", 				""),
-	Array("445", 				"Samba (Windows Network File Share)", 	""),
-	Array("21", 				"FTP", 									""),
+	Array("80", 				"Internet Connection", 						"google.com"),
+	Array("80",				"HTTP", 							""),
+//	Array("postfix",		 	"Postfix", 							""),
+	Array("cron",				"Cron",								""),
+//	Array("3306", 				"MySQL", 							""),
+	Array("445",				"Samba", 							""),
+//	Array("32400",				"Plex",								"localhost"),
+//	Array("21", 				"FTP", 								""),
 	Array("22", 				"Internal SSH", 						""),
-	Array("62484", 				"External SSH", 						"home.bertelson.me"),
-	Array("9091", 				"Transmission", 						""),
-	Array("5000", 				"ZNC IRC server", 						""),
-	Array("8888", 				"BitTorrent Sync", 						""),
-	Array("tightvnc",		 	"TightVNC (Remote Desktop)", 			""),
-	Array("unbound", 			"Unbound (DNS Server)", 				""),
-	Array("emulationstation", 	"EmulationStation (Gaming)", 			"")
-
+	Array("22", 				"External SSH", 						"$hostname"),
+//	Array("transmission",			"Transmission", 						""),
+	Array("3389",		 	        "XRDP (Remote Desktop)",		 			""),
+	Array("apache2",			"Apache2",							""),
+	Array("openvpn",			"Openvpn",							""),
+//	Array("631",                        	"CUPS", 							""),
 );
 // Define Header Links
 $links = Array(
-	Array("Transmission", 		"/transmission/gui/"),
-	Array("BT Sync", 			"/btsync/gui/"),
-	Array("Jinzora", 			"/jinzora/"),
-	Array("TT-RSS", 			"/tt-rss"),
-	Array("Owncloud", 			"http://cloud.bertelson.me"),
-	Array("VNC", 				"/vnc/")
-);	
+	Array("Transmission",			"http:/\/torrent.$domain"),
+	Array("Webmail", 			"http:/\/mail.$domain"),
+	Array("Google Cloud Print", 		"https:/\/www.google.com/cloudprint?user=0#printers"),
+	Array("RPi1 Status", 			"http:/\/rpi1.$domain/"),
+	Array("RPi2 Status", 			"http:/\/rpi2.$domain"),
+	Array("net Status", 			"http:/\/server.$domain"),
+	Array("Gateway",			"https:/\/$domain:8080")
+);
+
+//Temperature readouts -- enter C or F (Default: C)
+$degtype = "F";
+$hostname = gethostname();
+
 function microtime_float()
 {
     list($usec, $sec) = explode(" ", microtime());
@@ -66,27 +74,27 @@ foreach ($services as $service) {
 		}
 
 		$fp = @fsockopen("$service[2]", $service[0], $errno, $errstr, 1);
-								
+
 		if ($fp) {
 			$status = "online";
 			fclose($fp);
 		}
-					
+
 		@fclose($fp);
 	} else{
 		exec("pgrep $service[0]", $output, $return);
-						
+
 		if ($return == 0) {
 			$status = "online";
 		}
 	}
-	
+
 	$data0 .= "<tr><td>$service[1]</td><td class='status-$status'>$status</td></tr>"; //'#FFC6C6' '#D9FFB3'
 }
 
 
 //GET SERVER LOADS
-$loadresult = @exec('uptime'); 
+$loadresult = @exec('uptime');
 preg_match("/averages?: ([0-9\.]+),[\s]+([0-9\.]+),[\s]+([0-9\.]+)/",$loadresult,$avgs);
 
 
@@ -96,8 +104,8 @@ preg_match("/averages?: ([0-9\.]+),[\s]+([0-9\.]+),[\s]+([0-9\.]+)/",$loadresult
   $uptime = $uptime[0].', '.$uptime[1];
 
 $data1 = "<tr><td>Load Average </td><td>$avgs[1], $avgs[2], $avgs[3]</td>\n";
-$data1 .= "<tr><td>Server Uptime</td><td>".$uptime."</td></tr>\n";  
-  
+$data1 .= "<tr><td>Server Uptime</td><td>".$uptime."</td></tr>\n";
+
 //GET MEMORY DATA
   $used = `free -m | grep "buffers/cache" | awk '{print $3}'`;
   $totalram = chop(`free -m | grep Mem | awk '{print $2}'`);
@@ -105,15 +113,17 @@ $data1 .= "<tr><td>Server Uptime</td><td>".$uptime."</td></tr>\n";
 
 $data1 .= "<tr><td>Memory In Use	</td><td>$usedram_percent% (".$used."MB/".$totalram."MB)</td></tr>\n";
 
-$rootfs = chop(`df -h | grep rootfs | awk '{ print $5}'`);
-$rootfssize = chop(`df -h | grep rootfs | awk '{print $2}'`);
-$rootfsused = chop(`df -h | grep rootfs | awk '{print $3}'`);
-$data1 .= "<tr><td>SD Card	</td><td>$rootfs ($rootfsused/$rootfssize)</td></tr>\n";
+$rootfs = chop(`df -h | grep $root_partition | awk '{ print $5}'`);
+$rootfssize = chop(`df -h | grep $root_partition | awk '{print $2}'`);
+$rootfsused = chop(`df -h | grep $root_partition | awk '{print $3}'`);
+$data1 .= "<tr><td>Root	</td><td>$rootfs ($rootfsused/$rootfssize)</td></tr>\n";
 
-$mediafs = chop(`df -h | grep sda1 | awk '{ print $5}'`);
-$mediafssize = chop(`df -h | grep sda1 | awk '{print $2}'`);
-$mediafsused = chop(`df -h | grep sda1 | awk '{print $3}'`);
-$data1 .= "<tr><td>Hard Disk	</td><td>$mediafs ($mediafsused/$mediafssize)</td></tr>\n";
+if (!empty($secondary)) {
+  $extfs = chop(`df -h | grep $secondary | awk '{ print $5}'`);
+  $extfssize = chop(`df -h | grep $secondary | awk '{print $2}'`);
+  $extfsused = chop(`df -h | grep $secondary | awk '{print $3}'`);
+  $data1 .= "<tr><td>Secondary	</td><td>$extfs ($extfsused/$extfssize)</td></tr>\n";
+}
 
   
 //get ps data
@@ -144,12 +154,12 @@ $cputemp= round(($cputemp), 1);
 
 $data1 .= "<tr><td>CPU Temp	</td><td>$cputemp $degrees</td></tr>\n";
 
-$gputemp= `/opt/vc/bin/vcgencmd measure_temp | sed 's/temp=//' | sed 's/.C//'`;
+$gputemp= `temp | sed 's/temp=/\/' | sed 's/.C/\/'`;
 if ($degtype == "F") {
 $gputemp = $gputemp*9/5+32;
 }
 $gputemp= round(($gputemp), 1);
-$data1 .= "<tr><td>GPU Temp	</td><td>$gputemp $degrees</td></tr>\n";
+//$data1 .= "<tr><td>GPU Temp</td><td>$gputemp $degrees</td></tr>\n";
 
 //Generate Links
 //<li><a href="/transmission">Transmission</a></li>
@@ -168,7 +178,7 @@ $linkdata .= "<li><a href='$link[1]'>$link[0]</a></li>";
     <!-- Basic Page Needs
   ================================================== -->
 	<meta charset="utf-8">
-	<title>Raspberry Homepage</title>
+	<title><?php echo $hostname; ?> Status</title>
 	
     <!-- Mobile Specific Metas
   ================================================== -->
@@ -180,7 +190,7 @@ $linkdata .= "<li><a href='$link[1]'>$link[0]</a></li>";
 	<link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/responsive.css">
 	<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
-	
+
 	<!--[if lt IE 8]>
        <div style=' clear: both; text-align:center; position: relative;'>
          <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
@@ -192,15 +202,15 @@ $linkdata .= "<li><a href='$link[1]'>$link[0]</a></li>";
 		<script src="js/html5.js"></script>
 		<script src="js/css3-mediaqueries.js"></script>
 	<![endif]-->
-	
-	<link href='./images/favicon.png' rel='icon' type='image/x-icon'/>    
+
+	<link href='./images/favicon.png' rel='icon' type='image/x-icon'/>
 </head>
 <body>
 <!--------------Header--------------->
 <div class="wrap-header">
-<header> 
+<header>
 	<div id="logo"><a href="#"><img src="./images/logo.png"/></a></div>
-	
+
 	<nav>
 		<ul>
 			<?php echo $linkdata; ?>
@@ -208,15 +218,15 @@ $linkdata .= "<li><a href='$link[1]'>$link[0]</a></li>";
 	</nav>
 </header>
 </div>
-			
+
 <!--------------Content--------------->
 <section id="content">
-	<div class="zerogrid">		
+	<div class="zerogrid">
 		<div class="row">
 			<div id="main-content">
 				<article style="margin-left: 20px;">
 					<div class="heading">
-						<h2>Service Status</h2>
+						<h2><?php echo $hostname; ?> Service Status</h2>
 					</div>
 					<div class="content">
 						<table style="width:500px;">
